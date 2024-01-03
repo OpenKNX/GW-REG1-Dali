f386f318ce6014a9719deec5fa8e7d4fbcb1331a
thewhobox <45664417+thewhobox@users.noreply.github.com>
2024-01-03 10:34:14 +0100

    added new-clone script

---
+++ Firmware/scripts/New-Clone.ps1
@@ -0,0 +1,10 @@
+Write-Host "Klone das Repo GW-REG1-Dali"
+
+git clone https://github.com/OpenKnx/GW-REG1-Dali
+
+Write-Host "Führe das Skript 'Restore-First.ps1' aus"
+
+.\scripts\Restore-First.ps1
+
+
+Write-Host "Fertig"
