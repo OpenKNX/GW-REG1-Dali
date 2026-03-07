2bcd19171af2d94cd8da8dec1e2bc7235c261a6a
thewhobox <45664417+thewhobox@users.noreply.github.com>
2026-03-07 14:33:57 +0100

    fix: build scripts

--- scripts/Build-Release.ps1
+++ scripts/Build-Release.ps1
@@ -34,7 +34,11 @@ if (!$?) { exit 1 }
 
 
 # Example call, the following 2 lines might be there multiple times for each firmware which should be built
-../OGM-Common/scripts/setup/reusable/Build-Step.ps1 release_DEVICE_UP1_GW_IR UP1-GW-IR uf2
+../OGM-Common/scripts/setup/reusable/Build-Step.ps1 release_REG1_V1 OAM-DaliGateway uf2
+if (!$?) { exit 1 }
+
+# Example call, the following 2 lines might be there multiple times for each firmware which should be built
+../OGM-Common/scripts/setup/reusable/Build-Step.ps1 release_REG1_REG1_LAN_TP_DALI OAM-DaliGateway esp32
 if (!$?) { exit 1 }
 
 # build firmware based on generated headerfile for SAMD
--- scripts/OpenKNX-Build-Settings.ps1
+++ scripts/OpenKNX-Build-Settings.ps1
@@ -1,4 +1,4 @@
 # set product names, allows mapping of (devel) name in Project to a more consistent name in release
-$settings = lib/OGM-Common/scripts/build/OpenKNX-Build-Settings.ps1 $args[0] "InfraredGateway" "InfraredGateway"
+$settings = lib/OGM-Common/scripts/build/OpenKNX-Build-Settings.ps1 $args[0] "DaliGateway" "DaliGateway"
 
 Return $settings
